eaef571c87e52bd26e7b29134ad62ab2c2108621
Andrew Cope <copephobia@gmail.com>
2022-01-15 15:06:57 -0500

    Add initial app delegate

---
+++ ntfy-ios/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  ntfy.sh
+//
+//  Created by Andrew Cope on 1/15/22.
+//
+
+import UIKit
+import Firebase
+import FirebaseMessaging
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions
+      launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    return true
+  }
+}
--- ntfy-ios/App/AppMain.swift
+++ ntfy-ios/App/AppMain.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct AppMain: App {
+    // Set App Delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
