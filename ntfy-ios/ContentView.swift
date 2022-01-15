cac1782671b0b9148b11e30a9933594142c2bee6
Andrew Cope <copephobia@gmail.com>
2022-01-15 14:30:37 -0500

    Initial Commit

---
+++ ntfy-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ntfy-ios
+//
+//  Created by Andrew Cope on 1/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ntfy-ios/ntfy_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ntfy_iosApp.swift
+//  ntfy-ios
+//
+//  Created by Andrew Cope on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ntfy_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
