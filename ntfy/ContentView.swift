41fa743eb559c39ce956b91e3bd4fa9f2cb277f2
Philipp Heckel <philipp.heckel@gmail.com>
2022-05-13 20:09:13 -0400

    Initial Commit

---
+++ ntfy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ntfy
+//
+//  Created by Philipp Heckel on 5/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ntfy/ntfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ntfyApp.swift
+//  ntfy
+//
+//  Created by Philipp Heckel on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ntfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
