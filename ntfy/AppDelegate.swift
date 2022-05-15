7def871bfe869f8f8091e6f25de3763faafc8c64
Philipp Heckel <philipp.heckel@gmail.com>
2022-05-15 14:00:56 -0400

    Notification modelling

--- ntfy/AppDelegate.swift
+++ ntfy/AppDelegate.swift
@@ -161,9 +161,9 @@ extension AppDelegate: MessagingDelegate {
 
     let dataDict: [String: String] = ["token": fcmToken ?? ""]
     NotificationCenter.default.post(
-      name: Notification.Name("FCMToken"),
-      object: nil,
-      userInfo: dataDict
+        name: UserNotifications.Notification.Name("FCMToken"),
+        object: nil,
+        userInfo: dataDict
     )
     // TODO: If necessary send token to application server.
     // Note: This callback is fired at each app startup and whenever a new token is generated.
